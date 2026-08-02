8be8084ee5edb1e9dd87c932d689db132644e290
desertmilk <deserthideout@tutamail.com>
2026-08-02 12:33:47 +0000

    Fix CMake configure (add project/find_package) and make hr_time cross-platform

--- src/Tools/hr_time.cpp
+++ src/Tools/hr_time.cpp
@@ -1,25 +1,53 @@
 #include "hr_time.h"
 
+#ifdef Q_OS_WIN
+
+double CStopWatch::LIToSecs( LARGE_INTEGER & L) {
+        return ((double)L.QuadPart /(double)frequency.QuadPart);
+}
+
+CStopWatch::CStopWatch(){
+        timer.start.QuadPart=0;
+        timer.stop.QuadPart=0;
+        QueryPerformanceFrequency( &frequency );
+}
+
+void CStopWatch::startTimer( ) {
+    QueryPerformanceCounter(&timer.start);
+}
+
+void CStopWatch::stopTimer( ) {
+    QueryPerformanceCounter(&timer.stop);
+}
+
+
+double CStopWatch::getElapsedTime() {
+        LARGE_INTEGER time;
+        time.QuadPart = timer.stop.QuadPart - timer.start.QuadPart;
+    return LIToSecs( time) ;
+}
+
+#else
+
 #include <chrono>
 
-CStopWatch::CStopWatch()
-{
-    startTime = std::chrono::steady_clock::time_point{};
-    stopTime = std::chrono::steady_clock::time_point{};
+CStopWatch::CStopWatch(){
+    timer.start = std::chrono::steady_clock::time_point();
+    timer.stop = std::chrono::steady_clock::time_point();
 }
 
-void CStopWatch::startTimer()
-{
-    startTime = std::chrono::steady_clock::now();
+void CStopWatch::startTimer() {
+    timer.start = std::chrono::steady_clock::now();
 }
 
-void CStopWatch::stopTimer()
-{
-    stopTime = std::chrono::steady_clock::now();
+void CStopWatch::stopTimer() {
+    timer.stop = std::chrono::steady_clock::now();
 }
 
-double CStopWatch::getElapsedTime()
-{
-    const auto elapsed = std::chrono::duration<double>(stopTime - startTime);
-    return elapsed.count();
+double CStopWatch::getElapsedTime() {
+    using namespace std::chrono;
+    duration<double> diff = duration_cast<duration<double>>(timer.stop - timer.start);
+    return diff.count();
 }
+
+#endif
